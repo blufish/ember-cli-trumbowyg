37b93dc146666448abc22c223077fcc25f11b486
Blufish Technologies <brandon@blufish.me>
2018-04-21 01:18:07 -0700

    Updated for Ember 2.6+
    Was using deprecated didUpdateAttr parameters.

--- addon/components/trumbowyg-editor.js
+++ addon/components/trumbowyg-editor.js
@@ -50,29 +50,42 @@ export default Ember.Component.extend({
     this.$().trumbowyg('destroy');
   },
 
-  _isAttrChanged(attrs, attrName){
-    return Ember.get(attrs, `newAttrs.${attrName}.value`) !== Ember.get(attrs, `oldAttrs.${attrName}.value`);
+  
+  _isAttrChanged(attrName){
+    return this.get(attrName) !== this.get(`oldOptions.${attrName}`);
   },
 
   didInsertElement(){
     this._renderTrumbowyg();
   },
 
-  didUpdateAttrs(attrs) {
+  didReceiveAttrs() {
+    var oldAttrs = this.get('oldOptions');
+    this.get('optionNames').forEach((option) => {
+      //oldAttrs[option] = this.get(option);
+      Ember.set(oldAttrs, option, this.get(option));
+    });
+    Ember.set(oldAttrs, 'placeholder', this.get('placeholder'));
+    Ember.set(oldAttrs, 'disabled', this.get('disabled'));
+    this.set('oldOptions', oldAttrs);
+  },
+
+  didUpdateAttrs() {
     const optionsUpdated = this.get('optionNames')
-      .some(optionName => this._isAttrChanged(attrs, optionName));
-    const htmlUpdated = Ember.get(attrs, 'newAttrs.html.value') !== this.$().trumbowyg('html');
-    const disabledUpdated = this._isAttrChanged(attrs, 'disabled');
-    const placeholderUpdated = this._isAttrChanged(attrs, 'placeholder');
+      .some(optionName => this._isAttrChanged(optionName));
+    const htmlUpdated = this.get('html') !== this.$().trumbowyg('html');
+    const disabledUpdated = this._isAttrChanged('disabled');
+    const placeholderUpdated = this._isAttrChanged('placeholder');
+
+    if (htmlUpdated) {
+      this.$().trumbowyg('html', this.get('html'));
+    }
 
     if (optionsUpdated || placeholderUpdated) {
       this._destroyTrumbowyg();
       this._renderTrumbowyg();
     }
-
-    if (htmlUpdated) {
-      this.$().trumbowyg('html', this.get('html'));
-    }
+    
 
     if (disabledUpdated) {
       this._updateDisabled();
