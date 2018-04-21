f6937cc5d6f9a0ee2727cdf5b3c8b57f751750f7
Blufish Technologies <brandon@blufish.me>
2018-04-21 00:22:58 -0700

    Added jQuery-UI

--- index.js
+++ index.js
@@ -23,7 +23,24 @@ module.exports = {
     var trumbowygDist = path.join(app.bowerDirectory, 'trumbowyg', 'dist')
     var trumbowygLangsDist = path.join(trumbowygDist, 'langs');
     var trumbowygPluginsDist = path.join(trumbowygDist, 'plugins');
+    
+    [ 'version', 'data', 'disable-selection', 'focusable', 'escape-selector', 'form',
+      'ie', 'keycode', 'labels', 'jquery-1-7', 'plugin', 'safe-active-element',
+      'safe-blur', 'scroll-parent', 'tabbable', 'unique-id', 'widget'
+    ].forEach(function(module) {
+      app.import({
+        development: app.bowerDirectory + '/jquery-ui/ui/' + module + '.js',
+        production:  app.bowerDirectory + '/jquery-ui/ui/minified/' + module + '.min.js'
+      });
+    });
 
+    [ 'mouse', 'draggable', 'droppable', 'resizable' ].forEach(function(module) {
+      app.import({
+        development: app.bowerDirectory + '/jquery-ui/ui/widgets/' + module + '.js',
+        production:  app.bowerDirectory + '/jquery-ui/ui/widgets/minified/' + module + '.min.js'
+      });
+    });
+    
     app.import(path.join(trumbowygDist, 'trumbowyg.min.js'));
     app.import(path.join(trumbowygDist, 'ui/trumbowyg.min.css'));
     app.import(path.join(trumbowygDist, 'ui/icons.svg'), { destDir: 'assets/ui' });
@@ -55,3 +72,4 @@ module.exports = {
     });
   }
 };
+
